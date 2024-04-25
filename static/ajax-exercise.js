8761032aa0b248b82d0f9e1d933480208984ad09
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-04-24 20:38:14 -0400

    Developed first part of js front end for the weather service and order melons features

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,6 +4,12 @@
 
 function showFortune(evt) {
   // TODO: get the fortune and show it in the #fortune-text div
+
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((serverAnswer) => {                             //this is the answer from server with the data requested
+      document.getElementById('fortune-text').innerText = serverAnswer;
+    });
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -15,18 +21,49 @@ function showWeather(evt) {
 
   const url = '/weather.json';
   const zipcode = document.querySelector('#zipcode-field').value;
+  const queryString = 
 
   // TODO: request weather with that URL and show the forecast in #weather-info
+
+  fetch(url)
+  .then((response) => response.json())
+  .then((serverAnswer) => {
+    document.getElementById('weather-info').innerText = serverAnswer.forecast;
+    console.log(serverAnswer.forecast);
+    
+  });
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
+
 
 // PART 3: ORDER MELONS
 
 function orderMelons(evt) {
   evt.preventDefault();
+  
+  const url = '/order-melons.json';
+  const formInputs = {
+    qty: document.getElementById('qty-field').value,
+    melon_type: document.getElementById('melon-type-field').value
+  };  
 
-  // TODO: show the result message after your form
+  fetch(url, {
+    method:'POST',
+    body: JSON.stringify(formInputs),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((serverAnswer) => {
+      const code = serverAnswer.code;
+      const msg = serverAnswer.msg;
+      document.getElementById('order-status').innerText = `${code}. ${msg}`;  
+
+    });
+
+
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
